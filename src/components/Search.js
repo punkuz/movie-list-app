fa05d6c3b23c93a9ecaf545b29d79b7362cf602c
punkuz shah <punkuz.shah1@gmail.com>
2025-08-17 17:08:49 +0545

    feat: add input ref and focus effect to search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,18 @@
+import { useEffect, useRef } from "react";
+
 export default function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+  useEffect(() => {
+    inputEl.current.focus(); // Focus the input element when the component mounts
+  }, []);
   return (
     <input
       className='search'
       type='text'
       placeholder='Search movies...'
       value={query}
-      onChange={(e) => setQuery(e.target.value)} />
+      onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
+    />
   );
 }
